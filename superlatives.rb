5b56943b24cd881e5744719dbce6bacb98bbcf02
Rômulo Machado <romulo.machado5@gmail.com>
2015-03-13 16:46:07 -0300

    Add "It is so cool and so simple"

--- superlatives.rb
+++ superlatives.rb
@@ -8,8 +8,8 @@ class Superlative
       { quote: 'We are so proud to be a part of it.', file: 'we-are-so-proud.m4a' },
       { quote: 'It is really awesome!', file: 'it-is-really-awesome.m4a' },
       { quote: 'It is absolutely gorgeous!', file: 'it-is-absolutely-gorgeous.m4a' },
-      { quote: 'It has never been done before!', file: 'it-has-never-been-done-before.m4a' }
-      # { quote: '', file: '' },
+      { quote: 'It has never been done before!', file: 'it-has-never-been-done-before.m4a' },
+      { quote: 'It is so cool and so simple!', file: 'it-is-so-cool-and-so-simple.m4a' }
       # { quote: '', file: '' },
       # { quote: '', file: '' },
       # { quote: '', file: '' },
