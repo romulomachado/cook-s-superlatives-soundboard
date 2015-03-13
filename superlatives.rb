ed9a4858ae37440804a54382f1bf694e780572ef
Rômulo Machado <romulo.machado5@gmail.com>
2015-03-13 16:10:09 -0300

    Add "We are so proud to be a part of it."

--- superlatives.rb
+++ superlatives.rb
@@ -4,8 +4,8 @@ class Superlative
       { quote: 'Gorgeous new Design!', file: 'gorgeous-new-design.m4a' },
       { quote: 'Absolutely breathtaking!', file: 'absolutely-breathtaking.m4a' },
       { quote: 'This is phenomenal!', file: 'this-is-phenomenal.m4a' },
-      { quote: 'It is unbelievable momentum!', file: 'it-is-unbelievable-momentum.m4a' }
-      # { quote: '', file: '' },
+      { quote: 'It is unbelievable momentum!', file: 'it-is-unbelievable-momentum.m4a' },
+      { quote: 'We are so proud to be a part of it.', file: 'we-are-so-proud.m4a' }
       # { quote: '', file: '' },
       # { quote: '', file: '' },
       # { quote: '', file: '' },
