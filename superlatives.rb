275c0228409eb52ebc2763040146805e13a577a0
Rômulo Machado <romulo.machado5@gmail.com>
2015-03-13 16:01:56 -0300

    Add "It is unbelievable momentum!"

--- superlatives.rb
+++ superlatives.rb
@@ -3,8 +3,8 @@ class Superlative
     [
       { quote: 'Gorgeous new Design!', file: 'gorgeous-new-design.m4a' },
       { quote: 'Absolutely breathtaking!', file: 'absolutely-breathtaking.m4a' },
-      { quote: 'This is phenomenal!', file: 'this-is-phenomenal.m4a' }
-      # { quote: '', file: '' },
+      { quote: 'This is phenomenal!', file: 'this-is-phenomenal.m4a' },
+      { quote: 'It is unbelievable momentum!', file: 'it-is-unbelievable-momentum.m4a' }
       # { quote: '', file: '' },
       # { quote: '', file: '' },
       # { quote: '', file: '' },
