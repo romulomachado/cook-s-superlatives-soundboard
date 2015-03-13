29a7aa9004d34fad3060a32f4bfc446f6ba53da8
Rômulo Machado <romulo.machado5@gmail.com>
2015-03-13 16:23:24 -0300

    Add "It is absolutely gorgeous!"

--- superlatives.rb
+++ superlatives.rb
@@ -6,8 +6,8 @@ class Superlative
       { quote: 'This is phenomenal!', file: 'this-is-phenomenal.m4a' },
       { quote: 'It is unbelievable momentum!', file: 'it-is-unbelievable-momentum.m4a' },
       { quote: 'We are so proud to be a part of it.', file: 'we-are-so-proud.m4a' },
-      { quote: 'It is really awesome!', file: 'it-is-really-awesome.m4a' }
-      # { quote: '', file: '' },
+      { quote: 'It is really awesome!', file: 'it-is-really-awesome.m4a' },
+      { quote: 'It is absolutely gorgeous!', file: 'it-is-absolutely-gorgeous.m4a' }
       # { quote: '', file: '' },
       # { quote: '', file: '' },
       # { quote: '', file: '' },
