34ef02569fbcbf934d77aba0915d828a32e8ffc3
Rômulo Machado <romulo.machado5@gmail.com>
2015-03-13 15:56:50 -0300

    Add "This is phenomenal"

--- superlatives.rb
+++ superlatives.rb
@@ -3,7 +3,7 @@ class Superlative
     [
       { quote: 'Gorgeous new Design!', file: 'gorgeous-new-design.m4a' },
       { quote: 'Absolutely breathtaking!', file: 'absolutely-breathtaking.m4a' },
-      # { quote: '', file: '' },
+      { quote: 'This is phenomenal!', file: 'this-is-phenomenal.m4a' }
       # { quote: '', file: '' },
       # { quote: '', file: '' },
       # { quote: '', file: '' },
